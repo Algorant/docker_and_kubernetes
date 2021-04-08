7f054ed550c0ea1bd3fa40bcbc53c666ae7fb4cb
Skantastico <Skantastico@gmail.com>
2021-04-08 02:20:54 -0400

    added visits project, fixed simpleweb project errors

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  res.send('Monkeys are loose!');
 });
 
 app.listen(8080, () => {
